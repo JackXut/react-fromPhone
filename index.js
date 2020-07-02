054dc4e5c7380546d78c3d8d6d7a3e0c54f07c45
JackXut <codejs@i.ua>
2020-07-02 09:19:44 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import React-Dom from 'react-dom';
+// import React, { Component } from 'react';
+// import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       name: 'React'
+//     };
+//   }
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <Hello name={this.state.name} />
+//         <p>
+//           Start editing to see some magic happen :)
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
-render(<App />, document.getElementById('root'));
+// render(<App />, document.getElementById('root'));
