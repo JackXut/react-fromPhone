608b62c069b006b64951b6410c86bfb350cce1f1
JackXut <codejs@i.ua>
2020-07-03 16:47:55 +0300

    1 file modified

--- index.js
+++ index.js
@@ -61,11 +61,11 @@ function LogoutButton(props) {
   );
 }
 
-ReactDOM.render(
-  // Попробуйте заменить на isLoggedIn={true} и посмотрите на эффект.
-  <Greeting isLoggedIn={false} />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   // Попробуйте заменить на isLoggedIn={true} и посмотрите на эффект.
+//   <Greeting isLoggedIn={false} />,
+//   document.getElementById('root')
+// );
 
 
 class LoginControl extends React.Component {
@@ -101,6 +101,26 @@ class LoginControl extends React.Component {
     );
   }
 }
+
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Здравствуйте!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          У вас {unreadMessages.length} непрочитанных сообщений.
+        </h2>
+      }
+    </div>
+  );
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+// ReactDOM.render(
+//   `<Mailbox unreadMessages={messages} />`,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
   <LoginControl />,
