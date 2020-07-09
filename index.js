78bcac77134f3cf21702f18fe10a0e9c58cc62a8
JackXut <codejs@i.ua>
2020-07-09 12:49:39 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 // import react-dom from 'react-dom';
 // import React, { Component } from 'react';
 // import { render } from 'react-dom';
@@ -28,101 +28,132 @@ import ReactDOM from 'react-dom'
 
 // render(<App />, document.getElementById('root'));
 
-function UserGreeting(props) {
-  return <h1>С возвращением!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Войдите, пожалуйста.</h1>;
-}
 
 
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
+// function UserGreeting(props) {
+//   return <h1>С возвращением!</h1>;
+// }
+
+// function GuestGreeting(props) {
+//   return <h1>Войдите, пожалуйста.</h1>;
+// }
+
+// function Greeting(props) {
+//   const isLoggedIn = props.isLoggedIn;
+//   if (isLoggedIn) {
+//     return <UserGreeting />;
+//   }
+//   return <GuestGreeting />;
+// }
+
+// function LoginButton(props) {
+//   return <button onClick={props.onClick}>Войти</button>;
+// }
+
+// function LogoutButton(props) {
+//   return <button onClick={props.onClick}>Выйти</button>;
+// }
+
+// // ReactDOM.render(
+// //   // Попробуйте заменить на isLoggedIn={true} и посмотрите на эффект.
+// //   <Greeting isLoggedIn={false} />,
+// //   document.getElementById('root')
+// // );
+
+// class LoginControl extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleLoginClick = this.handleLoginClick.bind(this);
+//     this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//     this.state = { isLoggedIn: false };
+//   }
+
+//   handleLoginClick() {
+//     this.setState({ isLoggedIn: true });
+//   }
+
+//   handleLogoutClick() {
+//     this.setState({ isLoggedIn: false });
+//   }
+
+//   render() {
+//     const isLoggedIn = this.state.isLoggedIn;
+//     let button;
+//     if (isLoggedIn) {
+//       button = <LogoutButton onClick={this.handleLogoutClick} />;
+//     } else {
+//       button = <LoginButton onClick={this.handleLoginClick} />;
+//     }
+
+//     return (
+//       <div>
+//         <Greeting isLoggedIn={isLoggedIn} />
+//         {button}
+//       </div>
+//     );
+//   }
+// }
+
+// function Mailbox(props) {
+//   const unreadMessages = props.unreadMessages;
+//   return (
+//     <div>
+//       <h1>Здравствуйте!</h1>
+//       {unreadMessages.length > 0 && (
+//         <h2>У вас {unreadMessages.length} непрочитанных сообщений.</h2>
+//       )}
+//     </div>
+//   );
+// }
+
+// const messages = ["React", "Re: React", "Re:Re: React"];
+// // ReactDOM.render(
+// //   `<Mailbox unreadMessages={messages} />`,
+// //   document.getElementById('root')
+// // );
+
+// ReactDOM.render(<LoginControl />, document.getElementById("root"));
+
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>Вода закипит.</p>;
   }
-  return <GuestGreeting />;
+  return <p>Вода не закипит.</p>;
 }
 
-function LoginButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Войти
-    </button>
-  );
-}
-
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Выйти
-    </button>
-  );
-}
-
-// ReactDOM.render(
-//   // Попробуйте заменить на isLoggedIn={true} и посмотрите на эффект.
-//   <Greeting isLoggedIn={false} />,
-//   document.getElementById('root')
-// );
+clas
 
 
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
 
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
 
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
 
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
 
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </div>
-    );
-  }
-}
 
-function Mailbox(props) {
-  const unreadMessages = props.unreadMessages;
-  return (
-    <div>
-      <h1>Здравствуйте!</h1>
-      {unreadMessages.length > 0 &&
-        <h2>
-          У вас {unreadMessages.length} непрочитанных сообщений.
-        </h2>
-      }
-    </div>
-  );
-}
 
-const messages = ['React', 'Re: React', 'Re:Re: React'];
-// ReactDOM.render(
-//   `<Mailbox unreadMessages={messages} />`,
-//   document.getElementById('root')
-// );
 
-ReactDOM.render(
-  <LoginControl />,
-  document.getElementById('root')
-);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
